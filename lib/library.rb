613d80a56add7fccc01bb106b16918e8c28aed27
Logan Wilson <loganwilson777@gmail.com>
2023-02-24 10:57:01 -0700

    Adds add_author method, as well as publication time frame method, along with tests for each method

--- lib/library.rb
+++ lib/library.rb
@@ -1,3 +1,24 @@
 class Library
  
+  attr_reader :name,
+              :books,
+              :authors
+  def initialize(name)
+    @name = name
+    @books = []
+    @authors = []
+  end
+
+  def add_author(author)
+    @books << author.books
+    @authors << author
+    @authors.last
+  end
+
+  def publication_time_frame(author)
+    time_frame_hash = {start: "first", end: "last"}
+    time_frame_hash[:end] = author.books[1].publication_date
+    time_frame_hash[:start] = author.books[0].publication_date 
+    time_frame_hash
+  end
 end
---
+++ spec/library_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+RSpec.describe Library do
+  it 'exists' do
+    library = Library.new("Denver Library")
+    expect(library).to be_an_instance_of Library
+  end
+
+  it 'has atributes' do
+    library = Library.new("Denver Library")
+    expect(library.name).to eq("Denver Library")
+    expect(library.books).to eq([])
+    expect(library.authors).to eq([])
+  end
+
+  it 'can store authors' do
+    library = Library.new("Denver Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    expect(library.add_author(charlotte_bronte)).to eq(charlotte_bronte)
+  end
+
+  it 'can add books by author when author is added' do
+    library = Library.new("Denver Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "1847")
+    villette = charlotte_bronte.write("Villette", "1853")
+    library.add_author(charlotte_bronte)
+    expect(library.books).to eq([[jane_eyre,villette]])
+  end
+
+  it 'can have first and last book shown for an author' do
+    library = Library.new("Denver Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "1847")
+    villette = charlotte_bronte.write("Villette", "1853")
+    library.add_author(charlotte_bronte)
+    expect(library.publication_time_frame(charlotte_bronte)).to eq({:start=>"1847", :end=>"1853"})
+  end
+end

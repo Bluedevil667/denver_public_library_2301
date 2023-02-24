bcd3188a52e05c786fcca0172bcf7d4cec8e0362
Logan Wilson <loganwilson777@gmail.com>
2023-02-24 10:27:21 -0700

    adds write method, and interagtion test for all of the various cases

--- lib/author.rb
+++ lib/author.rb
@@ -7,4 +7,10 @@ class Author
     @last_name = characteristics[:last_name]
     @books = []
   end
+
+  def write(title, date)
+    book_hash = {title: title, publication_date: date}
+    @books << Book.new(book_hash)
+    @books.last
+  end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -11,4 +11,19 @@ RSpec.describe Author do
     expect(charlotte_bronte.last_name).to eq("Bronte")
     expect(charlotte_bronte.books).to eq([])
   end
+
+  it 'can write a book' do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    # require 'pry'; binding.pry
+    expect(jane_eyre.class).to eq Book
+    expect(jane_eyre.title).to eq("Jane Eyre")
+  end
+
+  it 'can have an array of books' do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    villette = charlotte_bronte.write("Villette", "1853")
+    expect(charlotte_bronte.books).to eq([jane_eyre,villette])
+  end
 end
